bc59ae1c8f7a0344826bf1b251bdc379f4455b10
aakash811 <125889687+aakash811@users.noreply.github.com>
2023-04-30 19:33:31 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,14 @@
+let count = 0;
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
+function increment() {
+  count++;
+  countEl.textContent = count;
+}
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  count = 0;
+  countEl.textContent = count;
+}
